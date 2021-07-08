27a09d53b0f6976c8e657b97ae4e39e77f9c47e6
Szymon Zygadliński <szygadlinski@gmail.com>
2021-07-08 19:52:57 +0200

    Add Header Component with styles

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux';
 
+import { AppBar, Toolbar, Typography, Link, Button } from '@material-ui/core';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import styles from './Header.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({className}) => (
   <div className={clsx(className, styles.root)}>
-    <h2>Header</h2>
-    {children}
+    <AppBar>
+      <Toolbar className={styles.toolbar}>
+        <Link href='/' className={styles.logo}>
+          <MenuBookIcon className={styles.icon} />
+          <Typography variant="h2" className={styles.title}>
+            Book Your Book!
+          </Typography>
+          <MenuBookIcon className={styles.icon} />
+        </Link>
+
+        <Button
+          className={styles.cart}
+          variant='contained'
+          component={NavLink}
+          to='/cart'
+          startIcon={<ShoppingCartIcon className={styles.icon} />}
+        >
+          <Typography variant="h5">
+            Cart
+          </Typography>
+        </Button>
+      </Toolbar>
+    </AppBar>
+
+    <Toolbar className={styles.toolbar}>
+      <Typography variant="h2" className={clsx(styles.title, styles.hidden)}>.</Typography>
+    </Toolbar>
   </div>
 );
 
 Component.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.node,
 };
 
 // const mapStateToProps = state => ({
