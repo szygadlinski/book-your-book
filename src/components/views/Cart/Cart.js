0cf13e220da16bdf92218b9e918544b5c14ac5a7
Szymon Zygadliński <szygadlinski@gmail.com>
2021-07-08 17:04:19 +0200

    Add basic views

---
+++ src/components/views/Cart/Cart.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+// import { connect } from 'react-redux';
+// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux';
+
+import styles from './Cart.module.scss';
+
+const Component = ({className, children}) => (
+  <div className={clsx(className, styles.root)}>
+    <h2>Cart</h2>
+    {children}
+  </div>
+);
+
+Component.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+// const mapStateToProps = state => ({
+//   someProp: reduxSelector(state),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   someAction: arg => dispatch(reduxActionCreator(arg)),
+// });
+
+// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+
+export {
+  Component as Cart,
+  //Container as Cart,
+  Component as CartComponent,
+};
---
+++ src/components/views/Order/Order.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+
+// import { connect } from 'react-redux';
+// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux';
+
+import styles from './Order.module.scss';
+
+const Component = ({className, children}) => (
+  <div className={clsx(className, styles.root)}>
+    <h2>Order</h2>
+    {children}
+  </div>
+);
+
+Component.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+// const mapStateToProps = state => ({
+//   someProp: reduxSelector(state),
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//   someAction: arg => dispatch(reduxActionCreator(arg)),
+// });
+
+// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+
+export {
+  Component as Order,
+  //Container as Order,
+  Component as OrderComponent,
+};
