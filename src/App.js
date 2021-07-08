9b2d368b406f0c09834f05ec1387d7c07f0f1127
Szymon Zygadliński <szygadlinski@gmail.com>
2021-07-08 17:42:00 +0200

    Add basic css variables & global styles

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import { NotFound } from './components/views/NotFound/NotFound';
 
 const theme = createTheme({
   palette: {
-    primary: { main: '#2B4C6F' },
+    primary: { main: '#0f5d42' },
   },
 });
 
