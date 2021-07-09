d68c4b5199dd03b3a29b5389cb99eea9c82fc698
Szymon Zygadliński <szygadlinski@gmail.com>
2021-07-09 04:18:52 +0200

    Fix initial state & map products to Homepage component

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -2,35 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux';
+import { connect } from 'react-redux';
+import { getAllProducts } from '../../../redux/productsRedux';
 
 import styles from './Homepage.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({className, products}) => (
   <div className={clsx(className, styles.root)}>
     <h2>Homepage</h2>
-    {children}
   </div>
 );
 
 Component.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.node,
+  products: PropTypes.array,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  products: getAllProducts(state),
+});
 
 // const mapDispatchToProps = dispatch => ({
 //   someAction: arg => dispatch(reduxActionCreator(arg)),
 // });
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
-  Component as Homepage,
-  //Container as Homepage,
+  //Component as Homepage,
+  Container as Homepage,
   Component as HomepageComponent,
 };
