e86b05c732ad115a83d265ea4d1f7c6e3bb7b26e
Szymon Zygadliński <szygadlinski@gmail.com>
2021-07-10 13:54:50 +0200

    Add content with styles to NotFound component

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
 
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux';
 
+import { Button } from '@material-ui/core';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import styles from './NotFound.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({className}) => (
   <div className={clsx(className, styles.root)}>
-    <h2>NotFound</h2>
-    {children}
+    <h2 className={styles.title}>
+      <MenuBookIcon className={styles.icon} />
+      Error 404 - there is nothing here... :(
+      <MenuBookIcon className={styles.icon} />
+    </h2>
+
+    <Button
+      className={styles.back}
+      variant='contained'
+      component={NavLink}
+      to='/'
+      startIcon={<ArrowBackIcon className={styles.icon} />}
+    >
+      Back to the main page
+    </Button>
   </div>
 );
 
 Component.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.node,
 };
 
 // const mapStateToProps = state => ({
