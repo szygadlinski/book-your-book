b1bcb9aaad1ceb5781f29f6cd7a4c1dd3a6cb7b8
Szymon Zygadliński <szygadlinski@gmail.com>
2021-07-15 03:23:30 +0200

    Add styles to OrderProduct component

--- src/components/features/OrderProduct/OrderProduct.js
+++ src/components/features/OrderProduct/OrderProduct.js
@@ -9,28 +9,31 @@ import styles from './OrderProduct.module.scss';
 
 const Component = ({className, title, cover, price, amount, comment}) => (
   <div className={clsx(className, styles.root)}>
-    <div className={styles.column}>
-      <div className={styles.photoWrapper}>
-        <img src={cover} alt={title} className={styles.photo} />
-      </div>
+    <div className={styles.photoWrapper}>
+      <img src={cover} alt={title} className={styles.photo} />
     </div>
 
-    <div className={styles.column}>
+    <div className={styles.info}>
       <div className={styles.title}>
         {title}
       </div>
 
       <div className={styles.price}>
-        Price: {price}$
+        Price: <span>{price}$</span>
       </div>
 
-      <div className={styles.price}>
-        Amount: {amount}
+      <div className={styles.amount}>
+        Amount: <span>{amount}</span>
       </div>
 
-      <div className={styles.price}>
-        Your comment: {comment}
-      </div>
+      {comment.length > 0
+        ?
+        <div className={styles.comment}>
+          Your comment: <span>{comment}</span>
+        </div>
+        :
+        ''
+      }
     </div>
   </div>
 );
