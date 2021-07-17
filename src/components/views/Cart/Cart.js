e1d3a7519abc23a9c4da7b853a52e2370854cf75
Szymon Zygadliński <szygadlinski@gmail.com>
2021-07-17 04:14:56 +0200

    Change Cart component

--- src/components/views/Cart/Cart.js
+++ src/components/views/Cart/Cart.js
@@ -15,62 +15,59 @@ import styles from './Cart.module.scss';
 import { CartProduct } from '../../features/CartProduct/CartProduct';
 import { calculateTotalPrice } from '../../../utils/calculateTotalPrice';
 
-const Component = ({ className, cartProducts }) => {
+const Component = ({ className, cartProducts }) => (
+  <div className={clsx(className, styles.root)}>
+    <h2 className={styles.title}>
+      <MenuBookIcon className={styles.icon} />
+        Your cart
+      <MenuBookIcon className={styles.icon} />
+    </h2>
 
+    {
+      cartProducts.length === 0
+        ?
+        <div className={styles.emptyCart}>
+          <h3>You did not add anything to cart yet :)</h3>
+          <Button
+            className={styles.back}
+            variant='contained'
+            component={NavLink}
+            to='/'
+            startIcon={<ArrowBackIcon className={styles.icon} />}
+          >
+            <Typography variant="h5">
+              Let&apos;s do some shopping!
+            </Typography>
+          </Button>
+        </div>
+        :
+        <div>
+          {cartProducts.map(product => (
+            <CartProduct key={product._id} {...product} />
+          ))}
 
-  return (
-    <div className={clsx(className, styles.root)}>
-      <h2 className={styles.title}>
-        <MenuBookIcon className={styles.icon} />
-          Your cart
-        <MenuBookIcon className={styles.icon} />
-      </h2>
+          <div className={styles.confirmOrder}>
+            <h5 className={styles.price}>
+              {`Total price: ${calculateTotalPrice(cartProducts)}$`}
+            </h5>
 
-      {
-        cartProducts.length === 0
-          ?
-          <div className={styles.emptyCart}>
-            <h3>You did not add anything to cart yet :)</h3>
             <Button
-              className={styles.back}
+              className={styles.orderButton}
               variant='contained'
               component={NavLink}
-              to='/'
-              startIcon={<ArrowBackIcon className={styles.icon} />}
+              to='/order'
+              startIcon={<ShoppingBasketIcon className={styles.icon} />}
             >
               <Typography variant="h5">
-                Let&apos;s do some shopping!
+                Confirm order
               </Typography>
             </Button>
           </div>
-          :
-          <div>
-            {cartProducts.map(product => (
-              <CartProduct key={product._id} {...product} />
-            ))}
+        </div>
+    }
+  </div>
+);
 
-            <div className={styles.confirmOrder}>
-              <h5 className={styles.price}>
-                {`Total price: ${calculateTotalPrice(cartProducts)}$`}
-              </h5>
-
-              <Button
-                className={styles.orderButton}
-                variant='contained'
-                component={NavLink}
-                to='/order'
-                startIcon={<ShoppingBasketIcon className={styles.icon} />}
-              >
-                <Typography variant="h5">
-                  Confirm order
-                </Typography>
-              </Button>
-            </div>
-          </div>
-      }
-    </div>
-  );
-};
 
 Component.propTypes = {
   className: PropTypes.string,
