f5da9cab4c0f4008ca7443a8571037ac410641b2
Szymon Zygadliński <szygadlinski@gmail.com>
2021-07-21 21:16:42 +0200

    Add access to remote database

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,9 @@
 let dbURI;
 
-dbURI = 'mongodb://localhost:27017/book-your-book';
+dbURI = process.env.NODE_ENV === 'production'
+  ?
+  'mongodb+srv://szymon-zygadlinski:Pfk6FxLGGr5AaTR@book-your-book.b1ou8.mongodb.net/book-your-book?retryWrites=true&w=majority'
+  :
+  'mongodb://localhost:27017/book-your-book';
 
 module.exports = dbURI;
