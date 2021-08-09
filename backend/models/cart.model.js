e1dd26b00aa77851f1b0268cac00e51fe72c0f0c
Szymon Zygadliński <szygadlinski@gmail.com>
2021-08-09 18:55:37 +0200

    Add server side validation to cart model

--- backend/models/cart.model.js
+++ backend/models/cart.model.js
@@ -4,8 +4,8 @@ const cartSchema = new mongoose.Schema({
   title: { type: String, required: true },
   cover: { type: String, required: true },
   price: { type: Number, required: true },
-  amount: { type: Number, required: true },
-  comment: { type: String },
+  amount: { type: Number, required: true, min: 1, max: 50 },
+  comment: { type: String, minlength: 0, maxlength: 500 },
 });
 
 module.exports = mongoose.model('Cart', cartSchema);
