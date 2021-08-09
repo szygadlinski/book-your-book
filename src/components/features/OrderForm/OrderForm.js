64ecb1707e399b26640cd0096b9df4247b2d1e5e
Szymon Zygadliński <szygadlinski@gmail.com>
2021-08-09 18:15:15 +0200

    Move OrderForm to separate file

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,36 +1,200 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import clsx from 'clsx';
 
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux';
+import { connect } from 'react-redux';
+import { getProductsFromCart, removeFromCartsDB } from '../../../redux/cartRedux';
+import { addToOrdersDB } from '../../../redux/ordersRedux';
 
+import { Button, Typography, FormControl, OutlinedInput } from '@material-ui/core';
+import ShopIcon from '@material-ui/icons/Shop';
 import styles from './OrderForm.module.scss';
 
-const Component = ({className, children}) => (
-  <div className={clsx(className, styles.root)}>
-    <h2>OrderForm</h2>
-    {children}
-  </div>
-);
+const Component = ({ className, orderedProducts, addToOrders, removeFromCart }) => {
+
+  const [order, setOrder] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    street: '',
+    code: '',
+    city: '',
+    country: '',
+    products: orderedProducts,
+  });
+
+  const changeOrder = event => {
+    setOrder({
+      ...order,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const history = useHistory();
+
+  const saveOrder = async event => {
+    event.preventDefault();
+
+    await addToOrders(order);
+    removeFromCart();
+
+    history.push('/');
+    window.location.reload();
+  };
+
+  return (
+    <div className={clsx(className, styles.root)}>
+      <form className={styles.confirmOrder} onSubmit={saveOrder}>
+        <FormControl>
+          <OutlinedInput
+            variant='outlined'
+            name='name'
+            required
+            className={styles.input}
+            placeholder='Your name'
+            value={order.name}
+            onChange={changeOrder}
+            inputProps={{
+              minLength: 5,
+              maxLength: 50,
+            }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <OutlinedInput
+            variant='outlined'
+            name='email'
+            required
+            type='email'
+            className={styles.input}
+            placeholder='Your email'
+            value={order.email}
+            onChange={changeOrder}
+            inputProps={{
+              minLength: 6,
+              maxLength: 100,
+            }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <OutlinedInput
+            variant='outlined'
+            name='phone'
+            required
+            type='tel'
+            className={clsx(styles.input, styles.phone)}
+            placeholder='Your phone'
+            value={order.phone}
+            onChange={changeOrder}
+            inputProps={{
+              minLength: 9,
+              maxLength: 18,
+            }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <OutlinedInput
+            variant='outlined'
+            name='street'
+            required
+            className={styles.input}
+            placeholder='Street'
+            value={order.street}
+            onChange={changeOrder}
+            inputProps={{
+              minLength: 3,
+              maxLength: 50,
+            }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <OutlinedInput
+            variant='outlined'
+            name='code'
+            required
+            className={styles.input}
+            placeholder='Postal code'
+            value={order.code}
+            onChange={changeOrder}
+            inputProps={{
+              minLength: 5,
+              maxLength: 6,
+            }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <OutlinedInput
+            variant='outlined'
+            name='city'
+            required
+            className={styles.input}
+            placeholder='City'
+            value={order.city}
+            onChange={changeOrder}
+            inputProps={{
+              minLength: 3,
+              maxLength: 50,
+            }}
+          />
+        </FormControl>
+
+        <FormControl>
+          <OutlinedInput
+            variant='outlined'
+            name='country'
+            required
+            className={styles.input}
+            placeholder='Country'
+            value={order.country}
+            onChange={changeOrder}
+            inputProps={{
+              minLength: 3,
+              maxLength: 50,
+            }}
+          />
+        </FormControl>
+
+        <Button
+          type='submit'
+          className={styles.orderButton}
+          variant='contained'
+          startIcon={<ShopIcon className={styles.icon} />}
+        >
+          <Typography variant="h5">
+                    Save order
+          </Typography>
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 Component.propTypes = {
   className: PropTypes.string,
-  children: PropTypes.node,
+  orderedProducts: PropTypes.array,
+  addToOrders: PropTypes.func,
+  removeFromCart: PropTypes.func,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  orderedProducts: getProductsFromCart(state),
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+  addToOrders: order => dispatch(addToOrdersDB(order)),
+  removeFromCart: () => dispatch(removeFromCartsDB()),
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as OrderForm,
-  //Container as OrderForm,
+  //Component as OrderForm,
+  Container as OrderForm,
   Component as OrderFormComponent,
 };
--- src/components/views/Order/Order.js
+++ src/components/views/Order/Order.js
@@ -1,249 +1,83 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { NavLink, useHistory } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
 
 import { connect } from 'react-redux';
-import { getProductsFromCart, removeFromCartsDB } from '../../../redux/cartRedux';
-import { addToOrdersDB } from '../../../redux/ordersRedux';
+import { getProductsFromCart } from '../../../redux/cartRedux';
 
-import { Button, Typography, FormControl, OutlinedInput } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import ShopIcon from '@material-ui/icons/Shop';
 import styles from './Order.module.scss';
 
 import { OrderProduct } from '../../features/OrderProduct/OrderProduct';
+import { OrderForm } from '../../features/OrderForm/OrderForm';
 import { calculateTotalPrice } from '../../../utils/calculateTotalPrice';
 
-const Component = ({ className, orderedProducts, addToOrders, removeFromCart }) => {
-
-  const [order, setOrder] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    street: '',
-    code: '',
-    city: '',
-    country: '',
-    products: orderedProducts,
-  });
-
-  const changeOrder = event => {
-    setOrder({
-      ...order,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const history = useHistory();
-
-  const saveOrder = async event => {
-    event.preventDefault();
-
-    await addToOrders(order);
-    removeFromCart();
-
-    history.push('/');
-    window.location.reload();
-  };
-
-  return (
-    <div className={clsx(className, styles.root)}>
-      <h2 className={styles.title}>
-        <MenuBookIcon className={styles.icon} />
+const Component = ({ className, orderedProducts }) => (
+  <div className={clsx(className, styles.root)}>
+    <h2 className={styles.title}>
+      <MenuBookIcon className={styles.icon} />
           Order Summary
-        <MenuBookIcon className={styles.icon} />
-      </h2>
+      <MenuBookIcon className={styles.icon} />
+    </h2>
 
-      {
-        orderedProducts.length === 0
-          ?
-          <div className={styles.emptyOrder}>
-            <h3>You did not order anything yet :)</h3>
-            <Button
-              className={styles.back}
-              variant='contained'
-              component={NavLink}
-              to='/'
-              startIcon={<ArrowBackIcon className={styles.icon} />}
-            >
-              <Typography variant="h5">
+    {
+      orderedProducts.length === 0
+        ?
+        <div className={styles.emptyOrder}>
+          <h3>You did not order anything yet :)</h3>
+          <Button
+            className={styles.back}
+            variant='contained'
+            component={NavLink}
+            to='/'
+            startIcon={<ArrowBackIcon className={styles.icon} />}
+          >
+            <Typography variant="h5">
                 Let&apos;s do some shopping!
-              </Typography>
-            </Button>
-          </div>
-          :
-          <div className={styles.notEmptyOrder}>
-            <div className={styles.orderedProducts}>
-              <Typography variant="h5" className={styles.sectionTitle}>
+            </Typography>
+          </Button>
+        </div>
+        :
+        <div className={styles.notEmptyOrder}>
+          <div className={styles.orderedProducts}>
+            <Typography variant="h5" className={styles.sectionTitle}>
                 Ordered products:
-              </Typography>
+            </Typography>
 
-              {orderedProducts.map(product => (
-                <OrderProduct key={product._id} {...product} />
-              ))}
+            {orderedProducts.map(product => (
+              <OrderProduct key={product._id} {...product} />
+            ))}
 
-              <h5 className={styles.price}>
-                {`Total price: ${calculateTotalPrice(orderedProducts)}$`}
-              </h5>
-            </div>
-
-            <div className={styles.shippingData}>
-              <Typography variant="h5" className={styles.sectionTitle}>
-                Shipping data:
-              </Typography>
-
-              <form className={styles.confirmOrder} onSubmit={saveOrder}>
-                <FormControl>
-                  <OutlinedInput
-                    variant='outlined'
-                    name='name'
-                    required
-                    className={styles.input}
-                    placeholder='Your name'
-                    value={order.name}
-                    onChange={changeOrder}
-                    inputProps={{
-                      minLength: 5,
-                      maxLength: 50,
-                    }}
-                  />
-                </FormControl>
-
-                <FormControl>
-                  <OutlinedInput
-                    variant='outlined'
-                    name='email'
-                    required
-                    type='email'
-                    className={styles.input}
-                    placeholder='Your email'
-                    value={order.email}
-                    onChange={changeOrder}
-                    inputProps={{
-                      minLength: 6,
-                      maxLength: 100,
-                    }}
-                  />
-                </FormControl>
-
-                <FormControl>
-                  <OutlinedInput
-                    variant='outlined'
-                    name='phone'
-                    required
-                    type='tel'
-                    className={clsx(styles.input, styles.phone)}
-                    placeholder='Your phone'
-                    value={order.phone}
-                    onChange={changeOrder}
-                    inputProps={{
-                      minLength: 9,
-                      maxLength: 18,
-                    }}
-                  />
-                </FormControl>
-
-                <FormControl>
-                  <OutlinedInput
-                    variant='outlined'
-                    name='street'
-                    required
-                    className={styles.input}
-                    placeholder='Street'
-                    value={order.street}
-                    onChange={changeOrder}
-                    inputProps={{
-                      minLength: 3,
-                      maxLength: 50,
-                    }}
-                  />
-                </FormControl>
-
-                <FormControl>
-                  <OutlinedInput
-                    variant='outlined'
-                    name='code'
-                    required
-                    className={styles.input}
-                    placeholder='Postal code'
-                    value={order.code}
-                    onChange={changeOrder}
-                    inputProps={{
-                      minLength: 5,
-                      maxLength: 6,
-                    }}
-                  />
-                </FormControl>
-
-                <FormControl>
-                  <OutlinedInput
-                    variant='outlined'
-                    name='city'
-                    required
-                    className={styles.input}
-                    placeholder='City'
-                    value={order.city}
-                    onChange={changeOrder}
-                    inputProps={{
-                      minLength: 3,
-                      maxLength: 50,
-                    }}
-                  />
-                </FormControl>
-
-                <FormControl>
-                  <OutlinedInput
-                    variant='outlined'
-                    name='country'
-                    required
-                    className={styles.input}
-                    placeholder='Country'
-                    value={order.country}
-                    onChange={changeOrder}
-                    inputProps={{
-                      minLength: 3,
-                      maxLength: 50,
-                    }}
-                  />
-                </FormControl>
-
-                <Button
-                  type='submit'
-                  className={styles.orderButton}
-                  variant='contained'
-                  startIcon={<ShopIcon className={styles.icon} />}
-                >
-                  <Typography variant="h5">
-                    Save order
-                  </Typography>
-                </Button>
-              </form>
-            </div>
+            <h5 className={styles.price}>
+              {`Total price: ${calculateTotalPrice(orderedProducts)}$`}
+            </h5>
           </div>
-      }
-    </div>
-  );
-};
+
+          <div className={styles.shippingData}>
+            <Typography variant="h5" className={styles.sectionTitle}>
+                Shipping data:
+            </Typography>
+
+            <OrderForm />
+          </div>
+        </div>
+    }
+  </div>
+);
 
 Component.propTypes = {
   className: PropTypes.string,
   orderedProducts: PropTypes.array,
-  addToOrders: PropTypes.func,
-  removeFromCart: PropTypes.func,
 };
 
 const mapStateToProps = state => ({
   orderedProducts: getProductsFromCart(state),
 });
 
-const mapDispatchToProps = dispatch => ({
-  addToOrders: order => dispatch(addToOrdersDB(order)),
-  removeFromCart: () => dispatch(removeFromCartsDB()),
-});
-
-const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps)(Component);
 
 export {
   Container as Order,
