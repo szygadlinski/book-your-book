31086a32aa3f59f8cea30e86577a750cda918cf9
Szymon Zygadliński <szygadlinski@gmail.com>
2021-08-09 21:09:17 +0200

    Add delay to NotFound component

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
@@ -8,25 +8,38 @@ import MenuBookIcon from '@material-ui/icons/MenuBook';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import styles from './NotFound.module.scss';
 
-const Component = ({ className }) => (
-  <div className={clsx(className, styles.root)}>
-    <h2 className={styles.title}>
-      <MenuBookIcon className={styles.icon} />
-        Error 404 - there is nothing here... :(
-      <MenuBookIcon className={styles.icon} />
-    </h2>
+const Component = ({ className }) => {
 
-    <Button
-      className={styles.back}
-      variant='contained'
-      component={NavLink}
-      to='/'
-      startIcon={<ArrowBackIcon className={styles.icon} />}
-    >
-      Back to the main page
-    </Button>
-  </div>
-);
+  const [render, setRender] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRender(true);
+    }, 5000);
+  });
+
+  if(!render) {
+    return null;
+  } else return (
+    <div className={clsx(className, styles.root)}>
+      <h2 className={styles.title}>
+        <MenuBookIcon className={styles.icon} />
+          Error 404 - there is nothing here... :(
+        <MenuBookIcon className={styles.icon} />
+      </h2>
+
+      <Button
+        className={styles.back}
+        variant='contained'
+        component={NavLink}
+        to='/'
+        startIcon={<ArrowBackIcon className={styles.icon} />}
+      >
+        Back to the main page
+      </Button>
+    </div>
+  );
+};
 
 Component.propTypes = {
   className: PropTypes.string,
