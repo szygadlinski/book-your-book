ae92f6e3bc56781eea7bc8d41678a3eac8ab7b6d
Szymon Zygadliński <szygadlinski@gmail.com>
2021-08-25 15:19:42 +0200

    Add static files to cache

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,16 @@
 
 var cacheName = 'book-your-book';
 var filesToCache = [
-  '/',
+  '/static/css/main.8542d530.chunk.css',
+  '/static/js/main.086530ea.chunk.js',
+  '/static/js/main.086530ea.chunk.js.map',
+  '/static/js/runtime-main.1e386c54.js',
+  '/static/js/runtime-main.1e386c54.js.map',
+  '/static/js/2.0da7c9d8.chunk.js',
+  '/static/js/2.0da7c9d8.chunk.js.map',
+  '/index.html',
+  '/static/css/main.8542d530.chunk.css.map',
+  '/static/js/2.0da7c9d8.chunk.js.LICENSE.txt',
 ];
 
 self.addEventListener('install', function(e) {
